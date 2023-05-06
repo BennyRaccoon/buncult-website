3b12f8607ee0a47298e7e31eefcc9b17c8d6293d
Alejandro Perez Ordoyo <theraccoonbenny@gmail.com>
2023-05-06 20:27:55 +0200

    First push of the site, site is now live.

---
+++ app/layout.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import localFont from 'next/font/local';
+import Link from 'next/link';
+import Image from 'next/image';
+
+const marketFont = localFont({
+    variable: '--font-market',
+    src: [
+        {
+            path: '../public/fonts/MarketAlt-Medium.woff2',
+            weight: 'normal',
+            style: 'normal'
+        },
+        {
+            path: '../public/fonts/Market-Thin.woff2',
+            weight: '300',
+            style: 'normal'
+        },
+        {
+            path: '../public/fonts/Market-Bold.woff2',
+            weight: 'bold',
+            style: 'normal'
+        }
+    ]
+});
+
+import './globals.css';
+
+export const metadata = {
+    title: 'The Bun Cult',
+    themeColor: '#F4C430',
+    openGraph: {
+        title: 'The Bun Cult',
+        url: 'https://buncult.com',
+        images: [
+            {
+                url: 'https://buncult.com/og.png',
+                width: 800,
+                height: 600
+            }
+        ]
+    },
+    robots: {
+        index: true
+    },
+    viewport: {
+        width: 'device-width',
+        initialScale: 1
+    }
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={marketFont.variable}>
+        <body>
+            <header>
+                <div className="header-container">
+                    <div className="header-background"></div>
+                    <div className="header-text"><Image src="/img/buncult.svg" width={300} height={50}/></div>
+                    <div className="header-bun"><Image src="/img/bun.png" width={300} height={300}/></div>
+                </div>
+                <div className='benny'><Image src="/img/_cafe.webp" width={256} height={293}/></div>
+                <nav>
+                    <Link className="navItem" href="https://twitter.com/bennyraccoon">twitter</Link>
+                    <Link className="navItem" href="https://www.instagram.com/bennyraccoon/">instagram</Link>
+                    <Link className="navItem" href="https://www.youtube.com/c/bennyraccoon">youtube</Link>
+                    <Link className="navItem" href="https://www.tiktok.com/@bennyraccoon">tiktok</Link>
+                </nav>
+            </header>
+            {children}
+        </body>
+    </html>
+  )
+}
---
+++ app/page.js
@@ -0,0 +1,58 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main>
+      <section>
+        <div className='newsletter-content'>
+          <h3>Check our newsletter!</h3>
+          <div id="mc_embed_signup">
+            <form action="https://gmail.us3.list-manage.com/subscribe/post?u=6ef6c6b7c05521adfa37922e8&amp;id=c157169537&amp;f_id=002a47e2f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_self">
+              <div id="mc_embed_signup_scroll">
+                <div className="mc-field-container">
+                  <div className="mc-field-group">
+                    <input type="email" placeholder='email@gmail.com' name="EMAIL" className="required email" id="mce-EMAIL" required/>
+                    <span id="mce-EMAIL-HELPERTEXT" className="helper_text"></span>
+                    <input type="submit" name="subscribe" id="mc-embedded-subscribe" className="button"/>
+                    <div style={{position: 'absolute', left: '-5000px', display: 'none'}} aria-hidden="true"><input type="text" name="b_6ef6c6b7c05521adfa37922e8_c157169537" tabIndex="-1" defaultValue=""/></div>
+                  </div>
+                </div>
+                <div id="mce-responses" className="clear foot">
+                  <div className="response" id="mce-error-response" style={{display: 'none'}}></div>
+                  <div className="response" id="mce-success-response" style={{display: 'none'}}></div>
+                </div>  
+                <div className="optionalParent">
+                  <div className="clear foot">
+                    <p className="brandingLogo">
+                      <Link href="http://eepurl.com/ioo50I" title="Mailchimp - email marketing made easy and fun">
+                        <Image src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_dark_dtp.svg" width={0} height={0}/>
+                      </Link>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+      <section className='two-content'>
+        <div className='content'>
+          <h2>A videogame!</h2>
+          <span>I am forging Benny the Raccoon's videogame! Ride along this adventure and get access to the secret content!</span>
+        </div>
+        <div className='content'>
+          <h2>Dungeons and dragons</h2>
+          <span>Jump into our original world and check the custom material! Character sheets, locations and exclusive adventures!</span>
+        </div>
+      </section>
+      <section>
+        <div className='last-ctas'>
+          <Link className='button-inverse' href="https://discord.gg/94khqNMdzT">Hop into our Discord, we have memes!</Link>
+          <Link className='button' href="https://www.patreon.com/buncult/membership">Support me on Patreon</Link>
+        </div>
+      </section>
+    </main>
+  )
+}
