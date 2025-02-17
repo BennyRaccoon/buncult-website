ceffbfcb4f91c350d4aae631457aa0a158ce5079
Alejandro Perez Ordoyo <81916422+BennyRaccoon@users.noreply.github.com>
2025-02-17 15:38:55 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -7,9 +7,7 @@ export default function Home() {
       <div className="header-container">
         <div className="header-background"></div>
         <div className="header-text"><Image alt="bun from the void" src="/img/buncult.svg" width={300} height={50} /></div>
-        <div className="header-bun"><Image alt="bun from the void" src="/img/bun.png" width={300} height={300} /></div>
       </div>
-      <div className='benny'><Image alt="benny drinking coffee" src="/img/_cafe.webp" width={256} height={293} /></div>
       <nav>
         <Link className="navItem" href="https://twitter.com/bennyraccoon">twitter</Link>
         <Link className="navItem" href="https://www.instagram.com/bennyraccoon/">instagram</Link>
@@ -18,17 +16,8 @@ export default function Home() {
       </nav>
     </header>
     <main>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/fIxz587N4RM?si=qE3xivihNlFzZs5f" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       <Link className="store-link" title='store' href='https://www.etsy.com/se-en/shop/TheBunCult'>Check my the store!</Link>
-      <section className='two-content'>
-        <div className='content'>
-          <h2>A videogame!</h2>
-          <span>I am forging Benny the Raccoon&apos;s videogame! Ride along this adventure and get access to the secret content!</span>
-        </div>
-        <div className='content'>
-          <h2>Dungeons and dragons</h2>
-          <span>Jump into the world of Galluvinchia and check the custom material! Character sheets, locations and exclusive adventures!</span>
-        </div>
-      </section>
       <section>
         <div className='last-ctas'>
           <Link className='button-inverse' href="https://discord.gg/94khqNMdzT">Hop into our Discord, we have memes!</Link>
